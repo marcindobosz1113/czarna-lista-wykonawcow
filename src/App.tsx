fa8f8c390da2865bc9976a40cb137dd7e5d8187a
marcindobosz <elomelo302@gmail.com>
2026-02-12 16:59:18 +0100

    project setup with router

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import './App.css'
+import { routeTree } from './routeTree.gen'
+
+const router = createRouter({ routeTree })
+
+function App() {
+  return <RouterProvider router={router} />
+}
+
+export default App
