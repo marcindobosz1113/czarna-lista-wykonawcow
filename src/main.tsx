2324670fb864edddec3b5bc5e50f340ac55c1499
marcindobosz <elomelo302@gmail.com>
2026-02-13 16:55:05 +0100

    app cleanup, packages installed

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from '@tanstack/react-router'
+import { router } from './app/router.ts'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './app/providers.ts'
+
+import './main.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 )
